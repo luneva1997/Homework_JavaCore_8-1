96af8cd0b301dcfccbd7a803a44a1a3b3a6095ff
luneva1997 <whatswrongnow@mail.ru>
2023-07-01 21:40:08 +0300

    commit

---
+++ Homework_JavaCore_8-1-client/src/Client.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8080;
+
+        try (Socket clientSocket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ){
+            out.println("Yulia");
+            String res = in.readLine();
+            System.out.println(res);
+
+        } catch (IOException ex){
+            ex.getMessage();
+        }
+    }
+}
